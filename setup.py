858e9fb748c542f04015be30e93c07c4c393106d
Jakub Turski <yacoob@ftml.net>
2024-07-28 16:44:11 +0100

    chore: s/poetry/ruff
    This includes:
    - moving the metadata to pyproject.toml
    - changing the Github workflow to match
    - adding a direnv file

--- setup.py
+++ setup.py
@@ -12,24 +12,9 @@ def find_stubs(package):
     return stubs
 
 
-with open("README.md", "r") as f:
-    LONG_DESCRIPTION = f.read()
-
 setup(
-    name="beancount-stubs",
-    version="0.1.3",
-    description="Stub files for the beancount package",
-    long_description=LONG_DESCRIPTION,
-    long_description_content_type="text/markdown",
     packages=["beancount-stubs"],
     package_data={
         "beancount-stubs": find_stubs("beancount-stubs"),
     },
-    author="Joshua Gilman",
-    author_email="joshuagilman@gmail.com",
-    url="https://github.com/jmgilman/beancount-stubs",
-    keywords=["beancount", "stubs", "mypy", "types"],
-    license="MIT",
-    install_requires=["beancount>=2.3.4"],
-    python_requires=">=3.8",
 )
